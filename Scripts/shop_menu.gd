7917d9b1d80a83cde0bd85d55abbbfb3b06ae5be
eColeye <81527120+eColeye@users.noreply.github.com>
2024-10-05 14:11:07 -0700

    given up on thingies

--- Scripts/shop_menu.gd
+++ Scripts/shop_menu.gd
@@ -1,21 +1,17 @@
 extends MenuButton
 
-#prefabs
-var house_asset = preload("res://Scenes/house.tscn")
-var mine_asset = preload("res://Scenes/mine.tscn")
-
 var asset_size = 0.35
 var asset_scale = Vector2(asset_size, asset_size) 
-var var_offset = 5
+var pos_offset = Vector2 ( 18, -7 )
 
 #Menu holder
 var popup_menu
 
 #Id { key : text, key : price }
 var items = {
-	1: {"text": "Buy House", "price": 100},	
-	2: {"text": "Buy Farm", "price": 200},
-	3: {"text": "Buy Mine", "price": 300},
+	1: {"text": "Buy House", "price": 100, "asset": preload("res://Scenes/house.tscn")},	
+	2: {"text": "Buy Farm", "price": 200, "asset": preload("res://Scenes/farm.tscn")},
+	3: {"text": "Buy Mine", "price": 300, "asset": preload("res://Scenes/mine.tscn")},
 }
 
 func _ready() -> void:
@@ -27,6 +23,8 @@ func _ready() -> void:
 	
 	#estabilishing what to do when item is selected
 	popup_menu.id_pressed.connect(on_item_selected)
+	self.mouse_entered.connect(on_mouse_entered)
+	self.mouse_exited.connect(on_mouse_exited)
 
 func update_items():
 	#Update prices by getting prices from their own respectfivly not going to be handled like this
@@ -42,44 +40,26 @@ func update_items():
 # Function to handle item selection
 func on_item_selected(id):
 	var item_text = self.get_popup().get_item_text(id-1)
-	match id:
-		1 : 
-			print("Tried to %s"%[item_text])
-			spawn_house()
-		2 : 
-			print("Tried to %s"%[item_text])
-		3 : 
-			print("Tried to %s"%[item_text])
-			spawn_mine()
+	#check if money allows
+	var spawned_asset = items[id]["asset"].instantiate()
+	if spawned_asset:
+		free_childen()
+		self.add_child(spawned_asset)
+		spawned_asset.position = spawned_asset.position - pos_offset
+		spawned_asset.scale = asset_scale
 	update_items()
 
 
-func spawn_house() -> void:
-	var house = house_asset.instantiate()
-	if house:
-		free_childen()
-		get_parent().add_child(house)
-		house.position = get_spawnposition()
-		house.scale = asset_scale
-	else:
-		print("Failed to instantiate house.")
-
-func spawn_mine() -> void:
-	var mine = mine_asset.instantiate()
-	if mine:
-		free_childen()
-		get_parent().add_child(mine)
-		mine.position = get_spawnposition()
-		mine.scale = asset_scale
-	else:
-		print("Failed to instantiate house.")
-
-#getting correct postion for Vector2 to spawn object at
-func get_spawnposition() -> Vector2:
-	var current_position = get_global_position()
-	var popup_menu_size = popup_menu.get_size()
-	return Vector2(current_position.x + (popup_menu_size.x / 2), current_position.y + (popup_menu_size.y) - var_offset)
 
 func free_childen():
-	for child in popup_menu.get_children():
+	print("freeing children")
+	for child in self.get_children():
+		print("freeing ", child)
 		child.queue_free()
+
+
+func on_mouse_entered():
+	print("Focusing")
+
+func on_mouse_exited():
+	print("stopped")
