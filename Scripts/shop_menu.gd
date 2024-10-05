d4bce585348a84e65d142967e7b8e30cd979cbfc
eColeye <81527120+eColeye@users.noreply.github.com>
2024-10-05 11:36:35 -0700

    spawning houses

--- Scripts/shop_menu.gd
+++ Scripts/shop_menu.gd
@@ -1,5 +1,8 @@
 extends MenuButton
 
+#prefabs
+var house_asset = preload("res://Scenes/house.tscn")
+
 #Menu holder
 var popup_menu
 
@@ -22,7 +25,7 @@ func _ready() -> void:
 
 func update_items():
 	#Update prices by getting prices from their own respectfivly not going to be handled like this
-	#THIS IS JUST TEST VALUES
+	#THIS IS JUST TEST VALUES 
 	items[1]["price"] += 200
 	items[2]["price"] += 200
 	items[3]["price"] += 200
@@ -37,8 +40,23 @@ func on_item_selected(id):
 	match id:
 		1 : 
 			print("Tried to %s"%[item_text])
+			spawn_house()
 		2 : 
 			print("Tried to %s"%[item_text])
 		3 : 
 			print("Tried to %s"%[item_text])
 	update_items()
+
+
+func spawn_house() -> void:
+	var house = house_asset.instantiate()
+	if house:
+		get_parent().add_child(house)
+		house.position = get_spawnposition()
+	else:
+		print("Failed to instantiate house.")
+
+func get_spawnposition() -> Vector2:
+	var current_position = get_global_position()
+	var popup_menu_size = popup_menu.get_size()
+	return Vector2(current_position.x + (popup_menu_size.x / 2), current_position.y + (popup_menu_size.y))
