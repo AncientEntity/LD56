396b261dd7d02600c628131b1c54c1700a3449da
eColeye <81527120+eColeye@users.noreply.github.com>
2024-10-06 14:07:36 -0700

    candy set up

--- Scripts/money_label.gd
+++ Scripts/money_label.gd
@@ -3,7 +3,7 @@ extends Label
 @onready var game_manager: Node = %GameManager
 
 func _ready() -> void:
-	self.text = ("dolla dolla bills is now %d"%[game_manager.money])
+	self.text = ("$%d"%[game_manager.money])
 
 func update_text():
-	self.text = ("dolla dolla bills is now %d"%[game_manager.money])
+	self.text = ("$%d"%[game_manager.money])
