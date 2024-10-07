4f68e6743003295bf62e76502dd8fe98b0b8fdbc
Ryan <ryanhasman10@hotmail.com>
2024-10-06 19:27:35 -0700

    fixed

--- Scripts/gameManager.gd
+++ Scripts/gameManager.gd
@@ -16,6 +16,25 @@ var tower_cost = 300
 var money = 1000000
 
 var roundState = CEnums.ERoundState.Invasion
+var roundTimeLeft
+var roundTime = 10
+var roundNumber = 0
 
 func update_lables():
 	money_lable.update_text()
+
+func _ready() -> void:
+	roundTimeLeft = roundTime
+
+func _process(delta: float) -> void:
+	handleWave(delta)
+
+func handleWave(delta : float):
+	roundTimeLeft -= delta
+	if roundTimeLeft <= 0:
+		roundNumber += 1
+		roundTimeLeft = roundTime
+		%WaveUI/WaveLabel.text = "Wave "+str(roundNumber)+" INCOMING"
+		%WaveUI/WaveLabel.visible = true
+	elif roundTimeLeft <= roundTime - 2.0:
+		%WaveUI/WaveLabel.visible = false
